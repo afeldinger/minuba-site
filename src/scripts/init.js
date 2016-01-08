65c3522179656e446bb7c2fd809834cc8ae2455e
Anders Feldinger <af@andersfeldinger.dk>
2016-01-08 11:17:08 +0100

    including modernizr in header-loaded init.js and corrected fixed nav clipping on ios

---
+++ src/scripts/init.js
@@ -0,0 +1,13 @@
+
+'use strict';
+
+// Modernizr (https://www.npmjs.org/package/browsernizr, https://github.com/jnordberg/browsernizr)
+require('browsernizr/lib/html5shiv');
+require('browsernizr/test/touchevents'); // Used to avoid hover states on touch-ready devices
+require('browsernizr/test/css/checked'); // Used to draw pretty checkboxes
+require('browsernizr/test/css/transforms'); // Used for positioning
+require('browsernizr/test/css/flexbox'); // Used for positioning
+require('browsernizr/test/css/flexboxlegacy'); // Used for positioning
+
+require('browsernizr');
+//var Modernizr = window.Modernizr = require('browsernizr');
