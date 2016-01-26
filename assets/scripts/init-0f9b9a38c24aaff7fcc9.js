f0fcc9147cd8b9f9de28b7b1ba6c5e79a0f433db
Anders Feldinger <af@andersfeldinger.dk>
2016-01-26 12:33:04 +0100

    Update 2016-01-26T11:32:59.956Z

---
+++ assets/scripts/init-0f9b9a38c24aaff7fcc9.js
@@ -0,0 +1,68 @@
+!function(e){function t(r){if(n[r])return n[r].exports;var o=n[r]={exports:{},id:r,loaded:!1};return e[r].call(o.exports,o,o.exports,t),o.loaded=!0,o.exports}var n={};return t.m=e,t.c=n,t.p="assets/scripts/",t(0)}([function(e,t,n){e.exports=n(1)},function(e,t,n){"use strict";n(2),n(5),n(14),n(15),n(31),n(32),n(33),n(37),svg4everybody()},function(e,t,n){var r,o=n(3);o||!function(t,n){function r(e,t){var n=e.createElement("p"),r=e.getElementsByTagName("head")[0]||e.documentElement;return n.innerHTML="x<style>"+t+"</style>",r.insertBefore(n.lastChild,r.firstChild)}function o(){var e=b.elements;return"string"==typeof e?e.split(" "):e}function i(e,t){var n=b.elements;"string"!=typeof n&&(n=n.join(" ")),"string"!=typeof e&&(e=e.join(" ")),b.elements=n+" "+e,f(t)}function a(e){var t=y[e[g]];return t||(t={},x++,e[g]=x,y[x]=t),t}function s(e,t,r){if(t||(t=n),d)return t.createElement(e);r||(r=a(t));var o;return o=r.cache[e]?r.cache[e].cloneNode():h.test(e)?(r.cache[e]=r.createElem(e)).cloneNode():r.createElem(e),!o.canHaveChildren||v.test(e)||o.tagUrn?o:r.frag.appendChild(o)}function c(e,t){if(e||(e=n),d)return e.createDocumentFragment();t=t||a(e);for(var r=t.frag.cloneNode(),i=0,s=o(),c=s.length;c>i;i++)r.createElement(s[i]);return r}function u(e,t){t.cache||(t.cache={},t.createElem=e.createElement,t.createFrag=e.createDocumentFragment,t.frag=t.createFrag()),e.createElement=function(n){return b.shivMethods?s(n,e,t):t.createElem(n)},e.createDocumentFragment=Function("h,f","return function(){var n=f.cloneNode(),c=n.createElement;h.shivMethods&&("+o().join().replace(/[\w\-:]+/g,function(e){return t.createElem(e),t.frag.createElement(e),'c("'+e+'")'})+");return n}")(b,t.frag)}function f(e){e||(e=n);var t=a(e);return!b.shivCSS||l||t.hasCSS||(t.hasCSS=!!r(e,"article,aside,dialog,figcaption,figure,footer,header,hgroup,main,nav,section{display:block}mark{background:#FF0;color:#000}template{display:none}")),d||u(e,t),e}var l,d,p="3.7.3",m=t.html5||{},v=/^<|^(?:button|map|select|textarea|object|iframe|option|optgroup)$/i,h=/^(?:a|b|code|div|fieldset|h1|h2|h3|h4|h5|h6|i|label|li|ol|p|q|span|strong|style|table|tbody|td|th|tr|ul)$/i,g="_html5shiv",x=0,y={};!function(){try{var e=n.createElement("a");e.innerHTML="<xyz></xyz>",l="hidden"in e,d=1==e.childNodes.length||function(){n.createElement("a");var e=n.createDocumentFragment();return"undefined"==typeof e.cloneNode||"undefined"==typeof e.createDocumentFragment||"undefined"==typeof e.createElement}()}catch(t){l=!0,d=!0}}();var b={elements:m.elements||"abbr article aside audio bdi canvas data datalist details dialog figcaption figure footer header hgroup main mark meter nav output picture progress section summary template time video",version:p,shivCSS:m.shivCSS!==!1,supportsUnknownElements:d,shivMethods:m.shivMethods!==!1,type:"default",shivDocument:f,createElement:s,createDocumentFragment:c,addElements:i};t.html5=b,f(n),"object"==typeof e&&e.exports&&(e.exports=b)}("undefined"!=typeof window?window:this,document),e.exports=r},function(e,t,n){var r=n(4),o="svg"===r.nodeName.toLowerCase();e.exports=o},function(e,t){var n=document.documentElement;e.exports=n},function(e,t,n){/*!
+	{
+	  "name": "Touch Events",
+	  "property": "touchevents",
+	  "caniuse" : "touch",
+	  "tags": ["media", "attribute"],
+	  "notes": [{
+	    "name": "Touch Events spec",
+	    "href": "http://www.w3.org/TR/2013/WD-touch-events-20130124/"
+	  }],
+	  "warnings": [
+	    "Indicates if the browser supports the Touch Events spec, and does not necessarily reflect a touchscreen device"
+	  ],
+	  "knownBugs": [
+	    "False-positive on some configurations of Nokia N900",
+	    "False-positive on some BlackBerry 6.0 builds – https://github.com/Modernizr/Modernizr/issues/372#issuecomment-3112695"
+	  ]
+	}
+	!*/
+var r=n(6),o=n(9),i=n(10);r.addTest("touchevents",function(){var e;if("ontouchstart"in window||window.DocumentTouch&&document instanceof DocumentTouch)e=!0;else{var t=["@media (",o.join("touch-enabled),("),"heartz",")","{#modernizr{top:9px;position:absolute}}"].join("");i(t,function(t){e=9===t.offsetTop})}return e})},function(e,t,n){var r=n(7),o=function(){};o.prototype=r,o=new o,e.exports=o},function(e,t,n){var r=n(8),o={_version:"3.2.0 (browsernizr 2.0.1)",_config:{classPrefix:"",enableClasses:!0,enableJSClass:!0,usePrefixes:!0},_q:[],on:function(e,t){var n=this;setTimeout(function(){t(n[e])},0)},addTest:function(e,t,n){r.push({name:e,fn:t,options:n})},addAsyncTest:function(e){r.push({name:null,fn:e})}};e.exports=o},function(e,t){var n=[];e.exports=n},function(e,t,n){var r=n(7),o=r._config.usePrefixes?" -webkit- -moz- -o- -ms- ".split(" "):[];r._prefixes=o,e.exports=o},function(e,t,n){var r=n(7),o=n(11),i=r.testStyles=o;e.exports=i},function(e,t,n){function r(e,t,n,r){var s,c,u,f,l="modernizr",d=i("div"),p=a();if(parseInt(n,10))for(;n--;)u=i("div"),u.id=r?r[n]:l+(n+1),d.appendChild(u);return s=i("style"),s.type="text/css",s.id="s"+l,(p.fake?p:d).appendChild(s),p.appendChild(d),s.styleSheet?s.styleSheet.cssText=e:s.appendChild(document.createTextNode(e)),d.id=l,p.fake&&(p.style.background="",p.style.overflow="hidden",f=o.style.overflow,o.style.overflow="hidden",o.appendChild(p)),c=t(d,e),p.fake?(p.parentNode.removeChild(p),o.style.overflow=f,o.offsetHeight):d.parentNode.removeChild(d),!!c}var o=(n(7),n(4)),i=n(12),a=n(13);e.exports=r},function(e,t,n){function r(){return"function"!=typeof document.createElement?document.createElement(arguments[0]):o?document.createElementNS.call(document,"http://www.w3.org/2000/svg",arguments[0]):document.createElement.apply(document,arguments)}var o=n(3);e.exports=r},function(e,t,n){function r(){var e=document.body;return e||(e=o(i?"svg":"body"),e.fake=!0),e}var o=n(12),i=n(3);e.exports=r},function(e,t,n){/*!
+	{
+	  "name": "CSS :checked pseudo-selector",
+	  "caniuse": "css-sel3",
+	  "property": "checked",
+	  "tags": ["css"],
+	  "notes": [{
+	    "name": "Related Github Issue",
+	    "href": "https://github.com/Modernizr/Modernizr/pull/879"
+	  }]
+	}
+	!*/
+var r=n(6),o=n(12),i=n(10);r.addTest("checked",function(){return i("#modernizr {position:absolute} #modernizr input {margin-left:10px} #modernizr :checked {margin-left:20px;display:block}",function(e){var t=o("input");return t.setAttribute("type","checkbox"),t.setAttribute("checked","checked"),e.appendChild(t),20===t.offsetLeft})})},function(e,t,n){/*!
+	{
+	  "name": "CSS Transforms",
+	  "property": "csstransforms",
+	  "caniuse": "transforms2d",
+	  "tags": ["css"]
+	}
+	!*/
+var r=n(6),o=n(16);r.addTest("csstransforms",function(){return-1===navigator.userAgent.indexOf("Android 2.")&&o("transform","scale(1)",!0)})},function(e,t,n){function r(e,t,n){return i(e,void 0,void 0,t,n)}var o=n(7),i=n(17);o.testAllProps=r,e.exports=r},function(e,t,n){function r(e,t,n,r,o){var f=e.charAt(0).toUpperCase()+e.slice(1),l=(e+" "+i.join(f+" ")+f).split(" ");return a(t,"string")||a(t,"undefined")?s(l,t,r,o):(l=(e+" "+c.join(f+" ")+f).split(" "),u(l,t,n))}var o=n(7),i=n(18),a=n(20),s=n(21),c=n(28),u=n(29);o.testAllProps=r,e.exports=r},function(e,t,n){var r=n(7),o=n(19),i=r._config.usePrefixes?o.split(" "):[];r._cssomPrefixes=i,e.exports=i},function(e,t){var n="Moz O ms Webkit";e.exports=n},function(e,t){function n(e,t){return typeof e===t}e.exports=n},function(e,t,n){function r(e,t,n,r){function f(){d&&(delete i.style,delete i.modElem)}if(r=c(r,"undefined")?!1:r,!c(n,"undefined")){var l=s(e,n);if(!c(l,"undefined"))return l}for(var d,p,m,v,h,g=["modernizr","tspan"];!i.style;)d=!0,i.modElem=a(g.shift()),i.style=i.modElem.style;for(m=e.length,p=0;m>p;p++)if(v=e[p],h=i.style[v],o(v,"-")&&(v=u(v)),void 0!==i.style[v]){if(r||c(n,"undefined"))return f(),"pfx"==t?v:!0;try{i.style[v]=n}catch(x){}if(i.style[v]!=h)return f(),"pfx"==t?v:!0}return f(),!1}var o=n(22),i=n(23),a=n(12),s=n(25),c=n(20),u=n(27);e.exports=r},function(e,t){function n(e,t){return!!~(""+e).indexOf(t)}e.exports=n},function(e,t,n){var r=n(6),o=n(24),i={style:o.elem.style};r._q.unshift(function(){delete i.style}),e.exports=i},function(e,t,n){var r=n(6),o=n(12),i={elem:o("modernizr")};r._q.push(function(){delete i.elem}),e.exports=i},function(e,t,n){function r(e,t){var n=e.length;if("CSS"in window&&"supports"in window.CSS){for(;n--;)if(window.CSS.supports(i(e[n]),t))return!0;return!1}if("CSSSupportsRule"in window){for(var r=[];n--;)r.push("("+i(e[n])+":"+t+")");return r=r.join(" or "),o("@supports ("+r+") { #modernizr { position: absolute; } }",function(e){return"absolute"==getComputedStyle(e,null).position})}}var o=n(11),i=n(26);e.exports=r},function(e,t){function n(e){return e.replace(/([A-Z])/g,function(e,t){return"-"+t.toLowerCase()}).replace(/^ms-/,"-ms-")}e.exports=n},function(e,t){function n(e){return e.replace(/([a-z])-([a-z])/g,function(e,t,n){return t+n.toUpperCase()}).replace(/^-/,"")}e.exports=n},function(e,t,n){var r=n(7),o=n(19),i=r._config.usePrefixes?o.toLowerCase().split(" "):[];r._domPrefixes=i,e.exports=i},function(e,t,n){function r(e,t,n){var r;for(var a in e)if(e[a]in t)return n===!1?e[a]:(r=t[e[a]],o(r,"function")?i(r,n||t):r);return!1}var o=n(20),i=n(30);e.exports=r},function(e,t){function n(e,t){return function(){return e.apply(t,arguments)}}e.exports=n},function(e,t,n){/*!
+	{
+	  "name": "Flexbox",
+	  "property": "flexbox",
+	  "caniuse": "flexbox",
+	  "tags": ["css"],
+	  "notes": [{
+	    "name": "The _new_ flexbox",
+	    "href": "http://dev.w3.org/csswg/css3-flexbox"
+	  }],
+	  "warnings": [
+	    "A `true` result for this detect does not imply that the `flex-wrap` property is supported; see the `flexwrap` detect."
+	  ]
+	}
+	!*/
+var r=n(6),o=n(16);r.addTest("flexbox",o("flexBasis","1px",!0))},function(e,t,n){/*!
+	{
+	  "name": "Flexbox (legacy)",
+	  "property": "flexboxlegacy",
+	  "tags": ["css"],
+	  "polyfills": ["flexie"],
+	  "notes": [{
+	    "name": "The _old_ flexbox",
+	    "href": "http://www.w3.org/TR/2009/WD-css3-flexbox-20090723/"
+	  }]
+	}
+	!*/
+var r=n(6),o=n(16);r.addTest("flexboxlegacy",o("boxDirection","reverse",!0))},function(e,t,n){var r=n(6),o=n(7),i=n(34),a=n(35),s=n(36);a(),s(i),delete o.addTest,delete o.addAsyncTest;for(var c=0;c<r._q.length;c++)r._q[c]();e.exports=r},function(e,t){var n=[];e.exports=n},function(e,t,n){function r(){var e,t,n,r,c,u,f;for(var l in o)if(o.hasOwnProperty(l)){if(e=[],t=o[l],t.name&&(e.push(t.name.toLowerCase()),t.options&&t.options.aliases&&t.options.aliases.length))for(n=0;n<t.options.aliases.length;n++)e.push(t.options.aliases[n].toLowerCase());for(r=s(t.fn,"function")?t.fn():t.fn,c=0;c<e.length;c++)u=e[c],f=u.split("."),1===f.length?i[f[0]]=r:(!i[f[0]]||i[f[0]]instanceof Boolean||(i[f[0]]=new Boolean(i[f[0]])),i[f[0]][f[1]]=r),a.push((r?"":"no-")+f.join("-"))}}var o=n(8),i=n(6),a=n(34),s=n(20);e.exports=r},function(e,t,n){function r(e){var t=i.className,n=o._config.classPrefix||"";if(a&&(t=t.baseVal),o._config.enableJSClass){var r=new RegExp("(^|\\s)"+n+"no-js(\\s|$)");t=t.replace(r,"$1"+n+"js$2")}o._config.enableClasses&&(t+=" "+n+e.join(" "+n),a?i.className.baseVal=t:i.className=t)}var o=n(6),i=n(4),a=n(3);e.exports=r},function(e,t,n){var r,o;!function(n,i){r=[],o=function(){return n.svg4everybody=i()}.apply(t,r),!(void 0!==o&&(e.exports=o))}(this,function(){/*! svg4everybody v2.0.0 | github.com/jonathantneal/svg4everybody */
+function e(e,t){if(t){var n=!e.getAttribute("viewBox")&&t.getAttribute("viewBox"),r=document.createDocumentFragment(),o=t.cloneNode(!0);for(n&&e.setAttribute("viewBox",n);o.childNodes.length;)r.appendChild(o.firstChild);e.appendChild(r)}}function t(t){t.onreadystatechange=function(){if(4===t.readyState){var n=document.createElement("x");n.innerHTML=t.responseText,t.s.splice(0).map(function(t){e(t[0],n.querySelector("#"+t[1].replace(/(\W)/g,"\\$1")))})}},t.onreadystatechange()}function n(n){function r(){for(var n,u,f=0;f<o.length;)if(n=o[f],u=n.parentNode,u&&/svg/i.test(u.nodeName)){var l=n.getAttribute("xlink:href");if(i&&(!a||a(l,u,n))){var d=l.split("#"),p=d[0],m=d[1];if(u.removeChild(n),p.length){var v=c[p]=c[p]||new XMLHttpRequest;v.s||(v.s=[],v.open("GET",p),v.send()),v.s.push([u,m]),t(v)}else e(u,document.getElementById(m))}}else f+=1;s(r,17)}n=n||{};var o=document.getElementsByTagName("use"),i="polyfill"in n?n.polyfill:/\bEdge\/12\b|\bTrident\/[567]\b|\bVersion\/7.0 Safari\b/.test(navigator.userAgent)||(navigator.userAgent.match(/AppleWebKit\/(\d+)/)||[])[1]<537,a=n.validate,s=window.requestAnimationFrame||setTimeout,c={};i&&r()}return n})}]);
